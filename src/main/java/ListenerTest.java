2e54abd1d00ba13ac890cdeee28140822a6e99b7
Andres <andresbolanosreyes@gmail.com>
2019-03-01 08:50:18 -0600

    added changes

--- src/main/java/ListenerTest.java
+++ src/main/java/ListenerTest.java
@@ -1,21 +1,28 @@
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.testng.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 
-public class ListenerTest implements ITestListener
+public class ListenerTest implements ITestListener, ISuiteListener
 {
+    public static Document document = new Document();
+    public static ArrayList<String> lista = new ArrayList<String>();
+
 
     public void onTestStart(ITestResult iTestResult) {
 
     }
 
     public void onTestSuccess(ITestResult iTestResult) {
-
+        lista.add("Descripcion: " + iTestResult.getMethod().getDescription() + "Result: Success");
     }
 
     public void onTestFailure(ITestResult iTestResult) {
+        lista.add("Descripcion: " + iTestResult.getMethod().getDescription() + "Result: Fail");
         String s = iTestResult.getMethod().getDescription();
         System.out.println(s);
     }
@@ -34,5 +41,37 @@ public class ListenerTest implements ITestListener
 
     public void onFinish(ITestContext iTestContext) {
 
+    }
+
+    public void onStart(ISuite iSuite) {
+
+    }
+
+    public void onFinish(ISuite iSuite) {
+        System.out.println("Description Suite");
+        document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        for(int i = 0; i <lista.size(); i++){
+            Chunk chunk = new Chunk(lista.get(i)+"\n", font);
+            Chunk chunk1 = new Chunk("\n", font);
+            try {
+                document.add(chunk);
+                document.add(chunk1);
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        document.close();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,91 @@
 public class Main {
+    Persona persona = new Persona();
+    public boolean pedirNombre(String nombre, String ap){
+        boolean respuesta = false;
+        if(nombre!=""){
+            if(nombre.length()<=11){
+               persona.setNombre(nombre);
+               respuesta = true;
+            }else{
+                System.out.println("nombre no puede ser mayor a 10 caracteres");
+            }
+        }else{
+            System.out.println("Nombre no puede ser vacio");
+        }
+        return respuesta;
+    }
+    public boolean pedirDPI(String dpi){ //funcione= 1234567891 falle=""  falle=12345678901234567  falle= 123456789012c
+        boolean ret = false;
+        if(dpi == ""){
+            System.out.println("Error, " + dpi + "is empty.");
+        } else if (!(dpi.length() == 13 )){
+            System.out.println("Error, " + dpi + "is not a DPI number.");
+        } else if (!dpi.matches(".\\d+.")){
+            System.out.println("Error, " + dpi + "contains invalid characters.");
+        }else{
+            ret = true;
+        }
+        return ret;
+    }
+
+    public boolean pedirEdad(int edad){ //funcione= 23  falle= 0  falle= -32  falle= 234
+        boolean ret = false;
+        if(edad == 0){
+            System.out.println("Error, " + edad + "cannot be 0.");
+        } else if (edad < 0){
+            System.out.println("Error, " + edad + "cannot be a negative number.");
+        } else if (edad > 140){
+            System.out.println("Error, " + edad + "is too big.");
+        }else{
+            ret = true;
+        }
+        return  ret;
+    }
+
+
+    public boolean pedirCorreo(String correo){ //funcione=danisnowman@gmail.com  falle=""  falle= danisnowman  falle= j@gmail.com
+        boolean ret = false;
+        if(correo == "") {
+            System.out.println("Error, " + correo + " is empty.");
+        } else if(correo.length()<10){
+            System.out.println("Error, " + correo + " is not a valid email.");
+        } else if(!correo.endsWith(".com") || !correo.endsWith(".edu") || !correo.endsWith(".io")){
+            System.out.println("Error, " + correo + " is not on a valid mail domain name.");
+        } else if(!correo.contains("@")){
+            System.out.println("Error, " + correo + " Invalid email address");
+        }else{
+            ret = true;
+        }
+        return ret;
+    }
+
+    public boolean pedirDireccion(String direccion){//funcione= Guatemala carretra a el salvador km 17 colinas de andalucia  falle=""  falle= guatemala  falle= carretra a el salvador km 17 colinas de andalucia
+        boolean ret = false;
+        if(direccion == "") {
+            System.out.println("Error, " + direccion + "is empty.");
+        } else if(!direccion.toLowerCase().contains("guatemala")){
+            System.out.println("Error, " + direccion + "is not located in Guatemala.");
+        } else if(direccion.length()<20){
+            System.out.println("Error, " + direccion + " is to ambiguous.");
+        }else{
+            ret = true;
+        }
+        return ret;
+    }
+
+    public boolean pedirTelefono(String telefono){ //funcione=30232185  falle=""  falle= 3023218  falle= 302321b5
+        boolean ret = false;
+        if(telefono == ""){
+            System.out.println("Error, " + telefono + "is too small to be a phone number.");
+        }
+        else if(telefono.length() < 8){
+            System.out.println("Error, " + telefono + "is too small to be a phone number.");
+        } else if (telefono.matches("-?\\d+")){
+            System.out.println("Error, " + telefono + "is not a phone number.");
+        }else{
+            ret = true;
+        }
+        return ret;
+    }
+
 }
